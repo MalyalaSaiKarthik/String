9e1d73019c38493aa78c2182a148697d46c64c14
MalyalaSaiKarthik <sai.karthik.malyala@gmail.com>
2023-01-24 12:16:27 -0600

    Initial Commit

---
+++ String.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var fact = "Swift is a type safe language"
+var dev = "Development of Swift began in 2010"
+var author = "Swift was created by Chris Lattner"
+
+print(fact.count)
+
+fact += ", it has a better memory management"
+print(fact)
+
+dev.append(" by Apple")
+print(dev)
+
+print(author.lowercased())
+
+print(author.uppercased())
